1acf0603e4149fb9bef621fb3fbb824849c5d988
HebaKassem <45446736+HebaKassem@users.noreply.github.com>
2019-04-07 21:16:50 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<head>
+<title>Home</title>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+<link href="layout/styles/layout.css" rel="stylesheet" type="text/css" media="all">
+</head>
+<body id="top" style="background-image:url('7.jpg') !important; background-size: cover; ">
+  <div id="pageintro"  class="hoc clear"> 
+    <article>
+      <h3 style="color: black; font-weight: bold;" class="heading">Largest Job Site On The Net</h3>
+      <p style="color: black; ">Land your dream job, improve your skills or even post a vacancy in an unlimited world of possibilities</p>
+      <p style="color: black; " class="font-x1 uppercase bold">Get involved</p>
+      <footer>
+        <ul style="color: black; " class="nospace inline pushright">
+          <li><a class="btn" href="loginview.php">Log in</a></li>
+          <li><a class="btn inverse"  href="signupview.php" >Sign up</a></li>
+        </ul>
+      </footer>
+    </article>
+  </div>
+ 
+</body>
+</html>
